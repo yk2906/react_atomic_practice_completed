b11932584e3f8d5c2c2007ab46cf4c509c54d50c
yk2906 <56224546+yk2906@users.noreply.github.com>
2021-11-06 19:53:23 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,17 +1,6 @@
 import { Router } from "./router/Router";
 import "./styles.css";
 
-const user = {
-  name: "ああああ",
-  image: "https://source.unsplash.com/NE0XGVKTmcA",
-  email: "12345@example.com",
-  phone: "080-1111-1111",
-  company: {
-    name: "テスト株式会社"
-  },
-  website: "https://google.com"
-};
-
 export default function App() {
   return <Router />;
 }
--- src/components/pages/Users.jsx
+++ src/components/pages/Users.jsx
@@ -1,11 +1,31 @@
 import styled from "styled-components";
 import { SearchInput } from "../molecules/SearchInput";
+import { UserCard } from "../organisms/user/UserCard";
+
+const users = [...Array(10).keys()].map((val) => {
+  return {
+    id: val,
+    name: `testuser-${val}`,
+    image: "https://source.unsplash.com/NE0XGVKTmcA",
+    email: "12345@example.com",
+    phone: "080-1111-1111",
+    company: {
+      name: "テスト株式会社"
+    },
+    website: "https://google.com"
+  };
+});
 
 export const Users = () => {
   return (
     <SContainer>
       <h2>ユーザー一覧</h2>
       <SearchInput />
+      <SUserArea>
+        {users.map((user) => (
+          <UserCard key={user.id} user={user} />
+        ))}
+      </SUserArea>
     </SContainer>
   );
 };
@@ -15,4 +35,12 @@ const SContainer = styled.div`
   flex-direction: column;
   align-items: center;
   padding: 24px;
+`;
+
+const SUserArea = styled.div`
+  padding-top: 40px;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-gap: 20px;
 `;
